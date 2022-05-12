eca1ff9d7a2b317821bc266c7f15c0612533153f
j-cool-morishita <morishita@j-cool.fm>
2022-05-12 10:04:36 +0900

    色々試した（練習2）

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function () {
+	$(".slide-body").slick({
+		console.log("hallo"),
+    auto: true,
+    // arrows: true,
+    // slideWidth: 240,
+    centerMode: true,
+    centerPadding: 150,
+    // slidesToShow: 4,
+    // minSlides: 1,
+    // maxSlides: 4,
+    // moveSlides: 1,
+    pause: 3000,
+    // pager: arguments,
+  });
+});
